f5b9338991e9a380aeb23d563aecf44d06066a1c
gliese-backup <glieseb+1@proton.me>
2024-12-15 07:22:32 +0000

    Layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,17 @@ import Form from "@/components/Form";
 
 const Home = () => {
   return (
-    <div>
+    <div className="p-4 w-full">
       <Header />
-      <Aside />
-      <Form />
+
+      <div className="grid grid-cols-8">
+        <div className="bg-green-300 col-span-2 hidden md:block">
+          <Aside />
+        </div>
+        <div className="md:bg-lime-400 md:col-span-6 col-span-full">
+          <Form />
+        </div>
+      </div>
     </div>
   );
 };
