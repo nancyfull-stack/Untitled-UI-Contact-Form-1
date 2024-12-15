144eed1d61ff29ef528ee940dcfc49c513de81f1
gliese-backup <glieseb+1@proton.me>
2024-12-15 07:11:41 +0000

    Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="mb-10 flex gap-2 items-center">
+      <img src="favicon.png" alt="Untitled UI Logo" className="w-10" />
+      <p className="font-bold">Untitled UI</p>
+    </header>
+  );
 };
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Header from "@/components/Header";
+import Aside from "@/components/Aside";
+import Form from "@/components/Form";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Header />
+      <Aside />
+      <Form />
+    </div>
+  );
 };
 
 export default Home;
