c076b3a2f640ac7d44b1475440777ee7e2a4fb8a
gliese-backup <glieseb+1@proton.me>
2024-12-15 07:49:50 +0000

    Showing contact info using array

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <Header />
 
       <div className="grid grid-cols-8">
-        <div className="col-span-2 hidden bg-green-300 md:block">
+        <div className="col-span-2 hidden md:block">
           <Aside />
         </div>
         <div className="col-span-full md:col-span-6 md:bg-lime-400">
