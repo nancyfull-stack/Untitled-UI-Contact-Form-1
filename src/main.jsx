66d59dd6167693b7af1b5149fdd4a4d9c29cdad7
gliese-backup <glieseb+1@proton.me>
2024-12-15 07:06:05 +0000

    Create @ alias

--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,13 @@
 // and then we can render anything using JSX
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "@/index.css";
+import Home from "@/pages/Home";
 
 const rootEl = document.querySelector("#root");
 const root = createRoot(rootEl);
 root.render(
   <React.StrictMode>
-    <h1>Hello</h1>
+    <Home />
   </React.StrictMode>
 );
