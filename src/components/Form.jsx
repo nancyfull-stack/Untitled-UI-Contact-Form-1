0d3af1562385f10c7df4de8e240f55f4f250a2b9
gliese-backup <glieseb+1@proton.me>
2024-12-15 08:34:31 +0000

    Form component Semantics

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
+const services = [
+  "Website Design",
+  "Content",
+  "UX Design",
+  "Strategy",
+  "User Research",
+  "Other",
+];
+
 const Form = () => {
   return (
     <div>
@@ -31,6 +40,27 @@ const Form = () => {
           placeholder="Tell us a little about your project..."
           className="h-24 w-full border-b border-zinc-600 p-1 placeholder-stone-600 md:bg-lime-400"
         />
+
+        <p className="my-3 text-stone-700">How can we help?</p>
+
+        <div className="mb-6 grid max-w-96 grid-cols-2">
+          {services.map((service) => {
+            return (
+              <label className="flex items-center gap-1" key={service}>
+                <input
+                  type="checkbox"
+                  name="Website Design"
+                  className="size-5"
+                />{" "}
+                {service}
+              </label>
+            );
+          })}
+        </div>
+
+        <button className="rounded-lg bg-stone-950 p-1 text-white">
+          Let's get started
+        </button>
       </div>
     </div>
   );
