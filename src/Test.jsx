53711c74d48e67976cea7450ae4a700a8875c712
nancyfull-stack <nancyirak291@gmail.com>
2024-12-22 06:26:13 +0000

    hh

--- src/Test.jsx
+++ src/Test.jsx
@@ -3,15 +3,9 @@ import React from "react";
 function Test() {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center">
-      <h1>Heading</h1>
-      <input
-        type="text"
-        name=""
-        id=""
-        className="rounded border-2 border-black"
-        placeholder="Enter some text"
-        onChange={(e) => console.log(e.target.value)}
-      />
+     <form className="flex min-h-screen w-full flex-col item-center justify-center">
+
+     </form>
     </div>
   );
 }
